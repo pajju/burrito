21db9de6fafefb992a4f4598ac78e0a673857d95
Philip Guo <philip@pgbovine.net>
2012-01-12 20:30:20 -0500

    minor comments

--- PythonLogger/pylogger.py
+++ PythonLogger/pylogger.py
@@ -6,7 +6,28 @@
 # _history_print to run on every type of command invocation, NOT just an
 # expression evaluation (which sys.displayhook) does.
 
+'''
+From Fernando Perez:
+
+  The user input is read through raw_input, or its lower-level partner,
+  the read method of sys.stdin.
+
+  IPython itself logs all that to an sqlite history database, so you
+  have all user inputs always, with timestamps and more.
+'''
+
+# Maybe I should instead just use IPython to keep the user input history!
+
+
 import sys, readline
+
+def my_read(s):
+  global _orig_stdin_read
+  _orig_stdin_read(s)
+
+_orig_stdin_read = sys.stdin.read
+sys.stdin.read = my_read
+
 
 def _history_print(arg):
   print 'history:'
