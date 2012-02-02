26134e80415b9f28d45b0d6f7b559217f131e6f1
Philip Guo <philip@pgbovine.net>
2012-02-02 12:44:39 -0800

    added launcher

---
+++ launch_activity_feed.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Make this executable and use this to launch the Activity Feed
+import os
+os.chdir('/home/researcher/burrito/post-integration/burrito-feed/')
+os.system('python burrito_feed.py')
+
